2465f36f1ab74e9c551b153a269c10fd3b27b74b
MD Alimuzzaman Haris <73691596+Az-Haris@users.noreply.github.com>
2022-01-27 01:42:10 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+
+    a=1;
+
+    while(a<=100)
+    {
+        printf("\t %d Az Haris \n", a);
+        a++;
+    }
+
+
+    return 0;
+}
+
+
+/*     Syntex ::
+
+
+    Initialization;
+
+    while(condition)
+    {
+        statement 1;
+        statement 2;
+        ............
+        ............
+        statement n;
+
+        increment / decrement;
+    }
+*/
